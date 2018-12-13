307e7350ab999638e1a5fe98f78a12df93919e41
BALAVIGNESHDOSTRIX <Balavignesh.m@mnw.co.in>
2018-12-13 14:35:44 +0530

    dostrix

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ app.use(express.static('./secutrixdapp'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/secutrixdapp/index.html'));
 });
