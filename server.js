115d7f12be8b21d714f7ff7db7daaee9803b917e
BALAVIGNESHDOSTRIX <Balavignesh.m@mnw.co.in>
2018-12-13 11:59:09 +0530

    final secutrixapp build files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./secutrixdapp'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/secutrixdapp/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000);
